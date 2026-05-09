122e913fb8588c8a69e6c3f97dc2495ac30de36e
Kazi.Saheb <kajisaheb809@gmail.com>
2026-05-09 22:04:56 +0600

    Update welcome.js

--- plugins/welcome.js
+++ plugins/welcome.js
@@ -27,7 +27,7 @@ async function handleJoinEvent(sock, id, participants) {
             forwardingScore: 1,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '0029VbC8MBZHrDZelHN6bJ2C@newsletter',
+                newsletterJid: '120363426421968955@newsletter',
                 newsletterName: 'Kazi-md-bot-4X',
                 serverMessageId: -1
             }
