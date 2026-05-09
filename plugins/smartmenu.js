966f9471a5b5a831822488624845d0407b279203
Kazi.Saheb <kajisaheb809@gmail.com>
2026-05-09 21:30:50 +0600

    Update smartmenu.js

--- plugins/smartmenu.js
+++ plugins/smartmenu.js
@@ -116,7 +116,7 @@ export default {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '0029VbC8MBZHrDZelHN6bJ2C@newsletter',
+                    newsletterJid: '120363426421968955@newsletter',
                     newsletterName: 'Kazi-md-bot-4X',
                     serverMessageId: -1
                 }
