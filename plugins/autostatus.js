e20292ba5d1cde1d6415abdb9ac0eace782c932e
Kazi.Saheb <kajisaheb809@gmail.com>
2026-05-09 21:53:42 +0600

    Update autostatus.js

--- plugins/autostatus.js
+++ plugins/autostatus.js
@@ -22,7 +22,7 @@ const channelInfo = {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '0029VbC8MBZHrDZelHN6bJ2C@newsletter',
+            newsletterJid: '120363426421968955@newsletter',
             newsletterName: 'Kazi-md-bot-4X',
             serverMessageId: -1
         }
