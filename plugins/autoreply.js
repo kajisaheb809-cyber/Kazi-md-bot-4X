82c6a0a20caaaf1998ef04845af69b1d27dfa08e
Kazi.Saheb <kajisaheb809@gmail.com>
2026-05-09 21:50:41 +0600

    Update autoreply.js

--- plugins/autoreply.js
+++ plugins/autoreply.js
@@ -52,8 +52,8 @@ export async function handleAutoReply(sock, chatId, message, userMessage) {
                         forwardingScore: 1,
                         isForwarded: true,
                         forwardedNewsletterMessageInfo: {
-                            newsletterJid: '120363406588763460@newsletter',
-                            newsletterName: 'ᴄʜʀɪs ɢᴀᴀᴊᴜ',
+                            newsletterJid: '120363426421968955@newsletter',
+                            newsletterName: 'Kazi-md-bot-4X',
                             serverMessageId: -1
                         }
                     }
