8df8f563893df5238f4f902f846b2650f099b6c8
Kazi.Saheb <kajisaheb809@gmail.com>
2026-05-17 21:27:14 +0600

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import 'dotenv/config';
-
-const token = process.env.BOT_TOKEN;
-
-const url = `https://api.telegram.org/bot${token}/getMe`;
-
-const res = await axios.get(url);
-console.log(res.data);
