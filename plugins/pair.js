893df223acbcee69e132f2f7ad05d19e7d17680f
Kazi.Saheb <kajisaheb809@gmail.com>
2026-05-20 00:18:16 +0600

    Update pair.js

--- plugins/pair.js
+++ plugins/pair.js
@@ -11,7 +11,7 @@ export default {
             forwardingScore: 1,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '0029VbC8MBZHrDZelHN6bJ2C@newsletter',
+                newsletterJid: '120363426421968955@newsletter',
                 newsletterName: 'Kazi-md-bot-4X',
                 serverMessageId: -1
             }
