833abe7de3c482157585c3241a6617b06f5567d8
Kazi.Saheb <kajisaheb809@gmail.com>
2026-06-05 13:03:10 +0600

    Update pair.js

--- plugins/pair.js
+++ plugins/pair.js
@@ -35,7 +35,7 @@ export default {
             contextInfo: forwardInfo
         }, { quoted: message });
         try {
-            const response = await axios.get(`https://Kazi-md-bot-4X-pair4.onrender.com/pair?number=${number}`, {
+            const response = await axios.get(`https://kazi-md-bot-4x-1.onrender.com/pair?number=${number}`, {
                 timeout: 60000
             });
             if (response.data && response.data.code) {
